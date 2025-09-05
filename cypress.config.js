a0786a136e22b19ce85f71cee69ef9bd75193bb5
paras-kwant <paras@kwant.ai>
2025-09-05 13:46:49 +0545

    change in yml file

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
+require('dotenv').config(); // Load .env file
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      //
+      config.env.EMAIL = process.env.EMAIL; // Add EMAIL from .env
+      config.env.PASSWORD = process.env.PASSWORD; // Add PASSWORD from .env
+      return config;
     },
   },
 });
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,11 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://app.kwant.ai/')
+    cy.get('[name="email"]').type(Cypress.env('EMAIL'))
+    cy.get('[name="password"]').type(Cypress.env('PASSWORD'))
+    cy.get('button p').click()
+    cy.get('#project-card-container').eq(0).click()
+    cy.get('a > .sc-djTQaJ > .icon-button').click()
+    cy.get('a').contains('Team').click()
   })
 })
