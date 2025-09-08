e42e71fb4ead482cb76516c2ae690b80bab7a380
paras-kwant <paras@kwant.ai>
2025-09-08 12:01:38 +0545

    change in api

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,24 @@
 const { defineConfig } = require("cypress");
-require('dotenv').config(); // Load .env file
+require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      //
-      config.env.EMAIL = process.env.EMAIL; // Add EMAIL from .env
-      config.env.PASSWORD = process.env.PASSWORD; // Add PASSWORD from .env
+      config.env.EMAIL = process.env.EMAIL;
+      config.env.PASSWORD = process.env.PASSWORD;
       return config;
     },
+    
+    defaultCommandTimeout: 60000,
+    requestTimeout: 60000,
+    responseTimeout: 60000,
+    pageLoadTimeout: 60000,
+    
+    baseUrl: 'https://app.kwant.ai',
+    
+    retries: {
+      runMode: 2,
+      openMode: 1
+    }
   },
 });
